6bc9113d7bb69c6243008c9b1acce93ba50ef290
KateCiz <ellie2joyful5@gmail.com>
2022-07-06 12:51:42 -0400

    current-user-session

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -36,6 +36,21 @@ router.delete(
       return res.json({ message: 'success' });
     }
   );
+
+  // Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+  
   
 
 module.exports = router;
