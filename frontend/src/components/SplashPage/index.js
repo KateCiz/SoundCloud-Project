4a354336c647134ada7ee54f24fb151a30415670
KateCiz <ellie2joyful5@gmail.com>
2022-08-30 21:00:25 -0400

    centered splash page elements and fit them better to a desktop screen

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,20 +2,20 @@ import './SplashPage.css'
 ;
 function SplashPage(){
     return (
-    <>
-    <div id="top-splash-image-div">
-        {/* <img id="top-splash-image" alt='girl-listening-to-music' src='https://res.cloudinary.com/dymmlu1dw/image/upload/v1661817134/soundcloud/pexels-andrea-piacquadio-813940_orqq1x.jpg'/> */}
+    <div className='splash-content'>
+        <div id="top-splash-image-div">
+            {/* <img id="top-splash-image" alt='girl-listening-to-music' src='https://res.cloudinary.com/dymmlu1dw/image/upload/v1661817134/soundcloud/pexels-andrea-piacquadio-813940_orqq1x.jpg'/> */}
+        </div>
+        <div id='splash-buttons'>
+            <input className='splash-buttons-content' type='search'></input>
+            <button className='splash-buttons-content'>Search</button>
+            <p className='splash-buttons-content'> OR </p>
+            <button className='splash-buttons-content'>Upload Your Own</button>
+        </div>
+        <div id='splash-call-to-action'>
+            <p id="call-to-action-text">Come On! What are you waiting for? Upload your music today!</p>
+        </div>
     </div>
-    <div>
-        <input type='search'></input>
-        <button>Search</button>
-        <p> OR </p>
-        <button>Upload Your Own</button>
-    </div>
-    <div id='splash-call-to-action'>
-        <p id="call-to-action-text">Come On! What are you waiting for? Upload your music today!</p>
-    </div>
-    </>
     )
 }
 
