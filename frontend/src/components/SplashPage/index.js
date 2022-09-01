ca47b0e696eb8ae02996ad098d5a5d56f10f9d80
KateCiz <ellie2joyful5@gmail.com>
2022-08-31 20:54:32 -0400

    logged in user splash page shows up and is styled

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,22 +1,45 @@
-import './SplashPage.css'
-;
+import { useSelector } from 'react-redux';
+import './SplashPage.css';
+
+
 function SplashPage(){
+    const loggedInUser = useSelector(state => state.session.user);
+    let content;
+
+    if(!loggedInUser) {
+        content = (
+            <div className='splash-content'>
+                <div id="top-splash-image-div">
+                </div>
+                <div id='splash-buttons'>
+                    <input className='splash-buttons-content' type='search'></input>
+                    <button className='splash-buttons-content'>Search</button>
+                    <p className='splash-buttons-content'> OR </p>
+                    <button className='splash-buttons-content'>Upload Your Own</button>
+                </div>
+                <div id='splash-call-to-action'>
+                    <p id="call-to-action-text">Come On! What are you waiting for? Upload your music today!</p>
+                </div>
+            </div> 
+        );
+    } else if(loggedInUser) {
+        content = (
+            <div className='splash-content'>
+                <div id="top-logged-in-splash-image-div"> 
+                </div>
+                <div id='splash-buttons'>
+                    <input className='splash-buttons-content' type='search'></input>
+                    <button className='splash-buttons-content'>Search</button>
+                    <p className='splash-buttons-content'> OR </p>
+                    <button className='splash-buttons-content'>Upload Your Own</button>
+                </div>
+            </div>
+        )
+    }
+
     return (
-    <div className='splash-content'>
-        <div id="top-splash-image-div">
-            {/* <img id="top-splash-image" alt='girl-listening-to-music' src='https://res.cloudinary.com/dymmlu1dw/image/upload/v1661817134/soundcloud/pexels-andrea-piacquadio-813940_orqq1x.jpg'/> */}
-        </div>
-        <div id='splash-buttons'>
-            <input className='splash-buttons-content' type='search'></input>
-            <button className='splash-buttons-content'>Search</button>
-            <p className='splash-buttons-content'> OR </p>
-            <button className='splash-buttons-content'>Upload Your Own</button>
-        </div>
-        <div id='splash-call-to-action'>
-            <p id="call-to-action-text">Come On! What are you waiting for? Upload your music today!</p>
-        </div>
-    </div>
-    )
+        content
+    );
 }
 
 export default SplashPage;
